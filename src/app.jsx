3edddbfc188c63ecc2e267401710f829e51e9894
Sancho <sanzarospanov@Sanchos-MacBook-Air.local>
2024-11-30 13:02:14 +0300

    init

--- src/app.jsx
+++ src/app.jsx
@@ -2,23 +2,5 @@ import { useEffect, useState } from 'react'
 import classes from './app.module.css'
 
 export const App = () => {
-	const [products, setProducts] = useState([])
-
-	useEffect(() => {
-		fetch('https://mocki.io/v1/09a5b348-3658-4582-863d-aae1a08e4147')
-			.then(response => response.json())
-			.then(loadedProducts => setProducts(loadedProducts))
-	}, [])
-
-	console.log(products)
-
-	return (
-		<div className={classes.app}>
-			{products.map(({ id, name, price }) => (
-				<div key={id}>
-					{name} - {price} rub
-				</div>
-			))}
-		</div>
-	)
+	return <div className={classes.app}>Hello World</div>
 }
