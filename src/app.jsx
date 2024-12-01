3ba604f1a88fbe96f5dda816790e4bedaa467966
Sancho <sanzarospanov@Sanchos-MacBook-Air.local>
2024-12-02 02:06:18 +0300

    finished task1 with json placeholder

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react'
 import classes from './app.module.css'
 
 export const App = () => {
-	return <div className={classes.app}>Hello World</div>
+	const [todos, setTodos] = useState([])
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then(response => response.json())
+			.then(todosArray => setTodos(todosArray))
+			.catch(error => console.error(error))
+	}, [])
+
+	return (
+		<div className={classes.app}>
+			<h1>Список задач</h1>
+			<ol className={classes.todosList}>
+				{todos.map(({ id, title }) => (
+					<li key={id} className={classes.todosItem}>
+						{title}
+					</li>
+				))}
+			</ol>
+		</div>
+	)
 }
