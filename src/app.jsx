b699ad4402d91c5ece6f268d1b62f24737fcf669
dispersionxm <ospanovsanjar1@gmail.com>
2025-01-02 01:35:56 +0300

    finished learning tailwind

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,8 @@
-import classes from './app.module.css'
-
 export const App = () => {
-	return <div className={classes.app}>Hello World</div>
+	return (
+		<div className={'text-3xl font-bold underline text-center w-dvw'}>
+			<div className={'text-blue-300 text-4xl mt-10'}>Hello World</div>
+			<div className={'circle-red m-auto mt-10'}></div>
+		</div>
+	)
 }
